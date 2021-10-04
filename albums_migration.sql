20ee9573e7fcd11b89c99203a0b307a131850296
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-10-04 13:33:07 -0500

    finished mysql basic statements exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,12 +2,12 @@ USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
-CREATE TABLE IF NOT EXISTS albums (
+CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR (35) NOT NULL,
-    name VARCHAR (65) NOT NULL,
+    artist VARCHAR (100) NOT NULL,
+    name VARCHAR (100) NOT NULL,
     release_date INT NOT NULL,
-    sales DECIMAL NOT NULL,
+    sales FLOAT(6, 2) NOT NULL,
     genre VARCHAR (65),
     PRIMARY KEY (id)
 )
