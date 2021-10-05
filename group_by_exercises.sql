cfbd794be869e8af25c1db1cb028c8a4968ccb7b
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-10-05 14:12:05 -0500

    finished mysql group by exercises

---
+++ group_by_exercises.sql
@@ -0,0 +1,25 @@
+use employees;
+
+select distinct title from titles;
+
+select last_name from employees
+where last_name like 'e%e'
+group by last_name;
+
+select first_name, last_name from employees
+where last_name like 'e%e'
+group by first_name, last_name;
+
+select last_name from employees
+where last_name like '%q%'
+    and last_name not like '%qu%'
+group by last_name;
+
+select last_name, count(last_name) from employees
+where last_name like '%q%'
+  and last_name not like '%qu%'
+group by last_name;
+
+select gender, count(gender) from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+group by gender;
